d985781c24d614fb798ba22fb521d2201265910e
rinwah <rinnuo041@gmail.com>
2026-04-30 10:51:48 -0400

    initial branch

---
+++ ai-cover-letter-tailor/src/main/java/com/rinwah/tailor/AiCoverLetterTailorApplication.java
@@ -0,0 +1,13 @@
+package com.rinwah.tailor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCoverLetterTailorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCoverLetterTailorApplication.class, args);
+	}
+
+}
---
+++ ai-cover-letter-tailor/src/test/java/com/rinwah/tailor/AiCoverLetterTailorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinwah.tailor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCoverLetterTailorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
